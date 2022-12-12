dcddde00df91292b92250c57121b72b662cae0cb
Ky3uH <aakuzin@gmail.com>
2022-12-12 16:29:15 +0300

    recover

---
+++ lesson_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру этого числа:
+
+Random random = new Random();
+
+int randomNumber = random.Next(10,100);
+
+int secondDigit = randomNumber % 10;
+int firstDigit = randomNumber / 10;
+
+Console.WriteLine(randomNumber);
+
+if(firstDigit>secondDigit)
+{
+    Console.WriteLine (firstDigit);
+}
+else
+{
+    Console.WriteLine (secondDigit);
+}
