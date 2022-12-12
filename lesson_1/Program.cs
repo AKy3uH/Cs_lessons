547e9d527747cccc898575e57931de6cd1e4e9a2
Ky3uH <aakuzin@gmail.com>
2022-12-12 16:26:19 +0300

    recover

---
+++ lesson_1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите число, квадрат которого нужен ");
+int number = int.Parse(Console.ReadLine());
+number = number * number;
+Console.Write("Квадрат Вашего числа ");
+Console.WriteLine(number);
